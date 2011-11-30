d887f2c48c247573a224bd60ab618a7dc6694f2c
rld <roguelikedev@gmail.com>
2011-11-30 11:52:24 -0800

    remove excessively long (and profane) utterance from line 106, Character.cs

--- HoS_proto/HoS_proto/Character.cs
+++ HoS_proto/HoS_proto/Character.cs
@@ -103,7 +103,7 @@ namespace HoS_proto
 
         public string Hail(Acter who)
         {
-            var rval = Quirks & Quirk.CASUAL ? "Heydiddle fuckwad progress" : "";
+            var rval = Quirks & Quirk.CASUAL ? "Hey" : "";
             if (Interactee != who) rval += ", " + who;
             Interactee = who;
             return rval;
